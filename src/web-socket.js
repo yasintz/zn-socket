0d4b3a8a4aaa8fa134d59b984f36a9f55f94c183
yasintz <yasinntazeoglu@gmail.com>
2020-08-14 14:55:42 +0300

    added log

--- src/web-socket.js
+++ src/web-socket.js
@@ -5,9 +5,11 @@ function initializeSocket(server) {
   io.on('connection', (socket) => {
     const connectLog = { type: 'connect', id: socket.id };
     socket.emit('log', connectLog);
+    console.log(connectLog);
     socket.on('emit', ({ event, arg }) => {
       const emitLog = { type: 'emit', id: socket.id, arg, event };
       socket.emit('log', emitLog);
+      console.log(emitLog);
       io.emit(event, arg);
     });
   });
