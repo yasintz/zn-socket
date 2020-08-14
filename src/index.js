749743ec355cab4fa1746d2841c295e1f2f80552
yasintz <yasinntazeoglu@gmail.com>
2020-08-14 12:33:35 +0300

    added cors for everywhere

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const morgan = require('morgan');
+var express = require('express');
 const http = require('http');
 const webSocket = require('./web-socket');
 const indexHtml = require('./index.html');
 
 const PORT = process.env.PORT || 9122;
 
-var express = require('express');
+var app = express()
+  .use(express.json())
+  .use(morgan('dev'))
+  .use(
+    cors({
+      origin: function (origin, callback) {
+        return callback(null, true);
+      },
+    })
+  );
 
-var app = express();
+app.get('/emitter', (req, res) => {
+  res.send(indexHtml(true));
+});
 
-app.use(express.json());
-
-app.use(morgan('dev'));
+app.get('/listener', (req, res) => {
+  res.send(indexHtml(false));
+});
 
 const server = http.createServer(app);
 
@@ -20,11 +32,4 @@ webSocket(server);
 server.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`> Ready on server http://localhost:${PORT}`);
-});
-
-app.get('/emitter', (req, res) => {
-  res.send(indexHtml(true));
-});
-app.get('/listener', (req, res) => {
-  res.send(indexHtml(false));
 });
